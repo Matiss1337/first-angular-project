acf73e346e32e077ecc28ea634e3206ac9cbd9e0
Matiss Judins <matissj1337@gmail.com>
2025-01-13 18:05:21 +0200

    added function and other stuff

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS} from "../dummy-users";
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css'
 })
-export class UserComponent {}
+export class UserComponent {
+    selectedUser = DUMMY_USERS[randomIndex];
+
+    get imagePath() {
+        return `assets/users/${this.selectedUser.avatar}`;
+    }
+
+    onSelectUser() {
+        const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+        this.selectedUser = DUMMY_USERS[randomIndex];
+    }
+}
 
 // Component can be made by CLI with command -  ng g componentName
+
+// get will allow to compute propert before passing it
+// imagePath() will be passed to element
