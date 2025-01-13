cf84b1962a33ab2d62e2f4cd3ecd3d3a69593bb7
Matiss Judins <matissj1337@gmail.com>
2025-01-13 07:16:06 +0200

    header complete

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -5,10 +5,13 @@ import {Component} from "@angular/core";
     // template: '<h1>The Header</h1>',
     standalone: true,
     templateUrl: './header.component.html',
-    // styleUrls: ['./header.component.css']
+    styleUrl: './header.component.css',
+    // styles: ['ng-container { display: block; }']
 })
 
 export class HeaderComponent {}
 
 /// Template can be used here without TS file, but not advise, rather use templateUrl
 /// standalone: true, is default in Angular 19+
+
+// Styles can be used here without CSS file, but not advise, rather use styleUrls, rather use styleUrls which takes array of style files or styleUrl if single css file
