da26ec63372904cdcc10c3a16a4fe9b77971e45d
Matiss Judins <matissj1337@gmail.com>
2025-01-21 20:25:16 +0200

    update

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,65 +7,19 @@ import {Component, Input, input, computed, Output, EventEmitter, output} from '@
     styleUrl: './user.component.css'
 })
 export class UserComponent {
-    @Input({required: true}) id!: string;
-    @Input({required: true}) avatar!: string; /// ! is used to tell that it will be initialized later
-    @Input({required: true}) name!: string; ///required will trow error if missed
-    @Output() select = new EventEmitter<string>(); ///Output is used to pass data to parent component /// Older approach
-    // select = output<string>(); ///Output is used to pass data to parent component and has eventEmiter automaticaly added /// Never feature
+    @Input({required: true}) user!: {
+        id: string;
+        name: string;
+        avatar: string;
+    };
 
-    // Signal aproach
-    // avatar = input.required<string>(); ///input<type>(initial value) if adding required we cant pass initial value
-    // name = input.required<string>();
-    // imagePath = computed(() => {
-    //     return `assets/users/${this.avatar()}`;
-    // })
+    @Output() select = new EventEmitter<string>(); ///Output is used to pass data to parent component /// Older approach
 
     get imagePath() {
-        return `assets/users/${this.avatar}`;
+        return `assets/users/${this.user.avatar}`;
     }
 
     onSelectUser() {
-        this.select.emit(this.id); ///emit is used to pass data to parent component
+        this.select.emit(this.user.id); ///emit is used to pass data to parent component
     }
 }
-
-
-// Static Version
-//
-// import { Component, signal, computed } from '@angular/core';
-//
-// import { DUMMY_USERS} from "../dummy-users";
-//
-// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-//
-// @Component({
-//     selector: 'app-user',
-//     standalone: true,
-//     templateUrl: './user.component.html',
-//     styleUrl: './user.component.css'
-// })
-// export class UserComponent {
-//     selectedUser = signal( DUMMY_USERS[randomIndex]); ///Sets state - SIGNAL
-//     imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`); ///Sets computed to use with Signal, which will run only if one of signals changes
-//
-//     ///Not the way with signals
-//     // get imagePath() {
-//     //     return `assets/users/${this.selectedUser().avatar}`;
-//     // }
-//
-//     onSelectUser() {
-//         const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-//         this.selectedUser.set(DUMMY_USERS[randomIndex]); ///sets state - SIGNAL
-//     }
-// }
-//
-// // Component can be made by CLI with command -  ng g componentName
-//
-// // get will allow to compute property before passing it
-// // imagePath() will be passed to element
-//
-//
-// // State
-// // Updates Dom when value changes causes rerendering of the component
-//
-// // If calling signal have to add() after defining signal
