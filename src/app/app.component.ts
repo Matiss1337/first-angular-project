b6b4abfe89777a8028543184727f4e7c5927062c
Matiss Judins <matissj1337@gmail.com>
2025-01-21 17:34:30 +0200

    added tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,25 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
+import {TasksComponent} from "./tasks/tasks.component";
 import {DUMMY_USERS} from "./dummy-users"; ///way to pass data to parent element
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports:    [HeaderComponent, UserComponent],
+    imports:    [HeaderComponent, UserComponent,TasksComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
 export class AppComponent {
     users = DUMMY_USERS; ///way to pass data to parent element
+    selectedUserId = 'u1';
+
+    get selectedUser() {
+        return this.users.find((user) => user.id === this.selectedUserId)!;
+    }
 
     onSelectUser(id: string) {
-        console.log(id);
+        this.selectedUserId = id;
     }
 }
-
-/// @component is ts decorator that allows you to mark a class as an Angular component and provide additional metadata that determines how the component should be processed, instantiated, and used at runtime.
-
-/// ts Decorator is a special kind of declaration that can be attached to a class declaration, method, accessor, property, or parameter.
-///Decorators use the form @expression, where expression must evaluate to a function that will be called at runtime with information about the decorated declaration.
-
-///selector: 'app-root' is the name of the component that will be used in the html file
-
-/// templateUrl: './app.component.html' is the path to the html file
-
-/// To use other component inside here, we need to import them and add to imports array
