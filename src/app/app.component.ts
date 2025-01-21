d7626c37327ff8cf04eae5034780f61e7b638e97
Matiss Judins <matissj1337@gmail.com>
2025-01-21 22:39:43 +0200

    conditional rendering, type vs interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserComponent} from "./user/user.component";
 import {TasksComponent} from "./tasks/tasks.component";
-import {DUMMY_USERS} from "./dummy-users"; ///way to pass data to parent element
+import {DUMMY_USERS} from "./dummy-users";
 
 @Component({
     selector: 'app-root',
@@ -12,8 +12,8 @@ import {DUMMY_USERS} from "./dummy-users"; ///way to pass data to parent element
     styleUrl: './app.component.css',
 })
 export class AppComponent {
-    users = DUMMY_USERS; ///way to pass data to parent element
-    selectedUserId = 'u1';
+    users = DUMMY_USERS
+    selectedUserId?: string;
 
     get selectedUser() {
         return this.users.find((user) => user.id === this.selectedUserId)!;
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,17 @@
 import {Component, Input, input, computed, Output, EventEmitter, output} from '@angular/core'; /// Input is decorator, input is function to pass props with signal
 
+// type User = {
+//     id: string;
+//     name: string;
+//     avatar: string;
+// }
+
+interface User {
+    id: string;
+    name: string;
+    avatar: string;
+} ///interface is used to define the type of object
+
 @Component({
     selector: 'app-user',
     standalone: true,
@@ -7,11 +19,7 @@ import {Component, Input, input, computed, Output, EventEmitter, output} from '@
     styleUrl: './user.component.css'
 })
 export class UserComponent {
-    @Input({required: true}) user!: {
-        id: string;
-        name: string;
-        avatar: string;
-    };
+    @Input({required: true}) user!: User; ///Input is used to pass data to child component /// Older approach};
 
     @Output() select = new EventEmitter<string>(); ///Output is used to pass data to parent component /// Older approach
 
